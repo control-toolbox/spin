31bfed2f9ae51411fcefe781fe19507351821459
Ibtissam Mimoun <ibtissam.mimoun22@gmail.com>
2024-07-01 11:00:01 +0200

    nouvelle version

--- essai.jl
+++ essai.jl
@@ -19,3 +19,33 @@ end
 direct_sol_iter = solve(ocp, print_level=0, grid_size= 200, init=initial_guess)
 
 
+
+#Homotopy : from the initial condition of [1, 0, 1, 0] to [0, 1, 0, 1]
+function f(x₀)
+    @def ocp begin
+        tf ∈ R, variable
+        t ∈ [ 0, tf ], time
+        x ∈ R⁴, state
+        u ∈ R, control
+        tf ≥ 0
+        -1 ≤ u(t) ≤ 1
+        x(0) == x₀
+        x(tf) == [0, 0, 0, 0]
+        ẋ(t) == [ (-Γ*x₁(t) -u(t)*x₂(t)), (γ*(1-x₂(t)) +u(t)*x₁(t)), (-Γ*x₃(t) -(1-ϵ)* u(t)*x₄(t)), (γ*(1-x₄(t)) +(1-ϵ)*u(t)*x₃(t))]
+        tf → min
+    end
+    return ocp
+end
+ocp1 = f([1, 0, 1, 0])
+sol1 = solve(ocp1,  grid_size=100)
+L = [sol1]
+for i in 1:10
+    x₀ = i/10 * [0, 1, 0, 1] + (1 - i/10) * [1, 0, 1, 0]
+    ocpi = f(x₀)
+    sol_i = solve(ocpi, grid_size=100, init=L[end])
+    global L 
+    push!(L, sol_i)
+end
+solution = L[end]
+solution.variable
+plot(solution)
