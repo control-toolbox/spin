3832fe23948dc9b716d6db29aa4be532add23a7f
Jean-Baptiste Caillau <jean-baptiste.caillau@univ-cotedazur.fr>
2024-07-04 23:16:56 +0200

    doc

---
+++ docs/make.jl
@@ -0,0 +1,18 @@
+using Documenter
+using DocumenterMermaid
+using OptimalControl
+
+makedocs(;
+    warnonly = [:cross_references, :autodocs_block],
+    sitename = "spin.jl",
+    format = Documenter.HTML(prettyurls = false),
+    pages = [
+        "Bisaturation" => "bisaturation.md",
+    ],
+    checkdocs=:none,
+)
+
+deploydocs(
+    repo = "github.com/control-toolbox/spin.jl.git",
+    devbranch = "main"
+)
