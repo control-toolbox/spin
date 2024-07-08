8bc7e19259ad51e42c051b1cfd72c10a8b6e5c0c
Ibtissam Mimoun <ibtissam.mimoun22@gmail.com>
2024-07-08 14:51:55 +0200

    update trisat*

--- src/tir_saturation.jl
+++ src/tir_saturation.jl
@@ -2,81 +2,97 @@ using OptimalControl
 using NLPModelsIpopt
 using Plots
 
-#include("bi_saturation.jl")
+include("bsat.jl")
 
 # Define the parameters of the problem
 Γ = 9.855e-2  
 γ = 3.65e-3  
 ϵ =0.1
-function F_2(q::Vector{Float64})
-    return [-Γ*q[1], γ*(1-q[2])]
-end 
-function F(q::Vector{Float64})
-    return [F_2(q[1:2]); F_2(q[3:4])]
-end 
-function G_2(q::Vector{Float64})
-    return [-q[2],q[1]]
+function F0i(q)
+    y, z = q
+    res = [-Γ*y, γ*(1-z)]
+    return res
 end
-function G(q::Vector{Float64})
-    return [G_2(q[1:2]); (1- ϵ)*G_2(q[3:4])]
+
+# idem for F1
+function F1i(q)
+    y, z = q
+    res = [-z, y]
+    return res
 end
+
+F0(q) = [ F0i(q[1:2]); F0i(q[3:4]) ]
+F1(q) = [ F1i(q[1:2]); (1 - ϵ) * F1i(q[3:4]) ]
 @def ocp_t begin
     tf ∈ R, variable
     t ∈ [0, tf], time
-    q ∈ R⁴, state
+    q = (y₁, z₁, y₂, z₂) ∈ R⁴, state
     u ∈ R, control
     tf ≥ 0
     -1 ≤ u(t) ≤ 1
     q(0) == [0, 1, 0, 1]
     q(tf) == [0, 0, 0, 0]
-    q̇(t) == F(q(t))+ G(q(t))*u(t)
+    q̇(t) == F0(q(t)) + u(t) * F1(q(t))
     tf → min
 end
 
-HF = Lift(F) 
-HG = Lift(G)
-H01  = @Lie { HF, HG }
-H001 = @Lie { HF, H01 }
-H101 = @Lie { HG, H01 }
+H0 = Lift(F0) 
+H1 = Lift(F1)
+H01  = @Lie { H0, H1 }
+H001 = @Lie { H0, H01 }
+H101 = @Lie { H1, H01 }
 us(x, p) = -H001(x, p) / H101(x, p)
 t = initial_g.times
 q = initial_g.state
 u = initial_g.control
 p = initial_g.costate
-φ(t) = HG(q(t), p(t))
+φ(t) = H1(q(t), p(t))
 uₘ = 1
+H1_plot = plot(t, φ,     label = "H₁(x(t), p(t))")
 
-function shoot(p0, tf , t1, t2, t3, z1, z2, z3)
-    s = zeros(eltype(p0), 32)
+function shoot(p0, tf , t1, t2, t3)
+    s = zeros(eltype(p0), 8)
     q0 = [0, 1, 0, 1]
-    q1 = z1[1]
-    q2 = z2[1]
-    q3 = z3[1]
-    p1 = z1[2]
-    p2 = z2[2]
-    p3 = z3[2]
-    u0 = sign(HG(q0, p0))
-    f0 = Flow(ocp, (q, p, tf) -> uₘ)
-    f1 = Flow(ocp, (q, p, tf) -> -uₘ)
-    fs = Flow(ocp, (q, p, tf) -> us(q, p))
-    qi1, pi1 = f1(0, q0, p0, t1)
-    qi2, pi2 = fs(t1, q1, p1, t2)
-    qi3, qi3 = f0(t2, q2, p2, t3)
-    qif, pif = fs(t3, q3, p3, tf)
-    s[1] = u0*HG(q0, p0) -1 
-    s[2] = HG(q1, p1)
+    f0 = Flow(ocp_t, (q, p, tf) -> uₘ)
+    f1 = Flow(ocp_t, (q, p, tf) -> -uₘ)
+    fs = Flow(ocp_t, (q, p, tf) -> us(q, p))
+    q1, p1 = f1(0, q0, p0, t1)
+    q2, p2 = fs(t1, q1, p1, t2)
+    p3, q3 = f0(t2, q2, p2, t3)
+    qf, pf = fs(t3, q3, p3, tf)
+    s[1] = -uₘ*H1(q0, p0) -1 
+    s[2] = H1(q1, p1)
     s[3] = H01(q1, p1)
-    s[4] = HG(q3, p3)
+    s[4] = H1(q3, p3)
     s[5] = H01(q3, p3)
     s[6] = q(t3)[3]
     s[7] = q(t3)[4]
-    s[8] = (pif(tf)[2] + pif(tf)[4])*γ -1
-    s[9:12] = qi1- q1
-    s[13:16] = pi1- p1
-    s[17:20] = qi2- q2
-    s[21:24] = pi2- p2
-    s[25:28] = qi3- q3
-    s[29:32] = pi3- p3
-
-    
+    s[8] = (pf[2] + pf[4])*γ -1
+    return s
 end
+t0 =0
+tol = 0.01
+t13 = [ elt for elt in t if abs(φ(elt)) < tol]
+i = 1
+t_l = []
+while(true)
+    if(t13[i+1] - t13[i] > 0.06)
+        break
+    else 
+        push!(t_l, t13[i])
+        i += 1
+    end
+end
+t_l 
+
+t1 = min(t_l...)
+t2 = max(t_l...)
+t3f = [elt for elt in t13 if elt > t2+0.1]
+t3 = min(t3f...)
+p0 = p(t0)
+q0 = [0, 1, 0, 1]
+tf = initial_g.variable
+using DifferentialEquations
+f1 = Flow(ocp_t, (q, p, tf) -> -uₘ)
+qi1, pi1 = f1(0, q0, p0, t1)
+shoot(p0, tf, t1, t2, t3)
